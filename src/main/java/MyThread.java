533a8afbcab41457d1ff64140223592e228733e8
Ostap Ptah <ostap.ptah@yandex.ua>
2019-08-21 13:36:55 +0300

    На основании предыдущего задания сделать запись списков логов в 3 потока 3 раза с промежутком 3 минуты  (10 х 3 х 3 = 90 дополнительных строк в файле)

--- src/main/java/MyThread.java
+++ src/main/java/MyThread.java
@@ -7,7 +7,7 @@ public class MyThread extends Thread {
     private static List<LogRecord> saveInfos = new ArrayList<>();
 
     @Override
-    public void run() {
+    public void run() { 
         for (int i = 0; i < 3; i++) {
 
             try (FileWriter fw = new FileWriter("C:/Users/Остап/IdeaProjects/learning/src/main/log.txt", true)) {
